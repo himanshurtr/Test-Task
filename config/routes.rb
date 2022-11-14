807de739e2ad79f83b7cd95f274d109b43f490e7
himanshu rathore <hrathore@technomatz.com>
2022-11-14 15:33:33 +0530

    test commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users, controllers: { registrations: 'users/registrations'}
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  
+end
